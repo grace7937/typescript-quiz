c60a0b9ea2745a99f7126ccf6a92af851a04ff33
grace7937 <eunchong.k153@gamil.com>
2021-02-16 18:34:04 +0900

    calculator-finished

--- calculator.ts
+++ calculator.ts
@@ -6,3 +6,17 @@ console.log(calculate('substract', 3, 1)); // 2
 console.log(calculate('multiply', 4, 2)); // 8
 console.log(calculate('divide', 4, 2)); // 2
 console.log(calculate('remainder', 5, 2)); // 1
+
+type Command = 'add' | 'substrat' | 'multiply' | 'divide' | 'remainder';
+
+function calculate(command: Command , firstNumber: number , secondNumber: number): number {
+  
+  switch(command) {
+    case 'add': return firstNumber + secondNumber;
+    case 'substract': return firstNumber - secondNumber;
+    case 'multiply': return firstNumber * secondNumber;
+    case 'divide': return firstNumber / secondNumber;
+    case 'remanider': return firstNumber % secondNumber;
+    default: throw Error('unknown command');
+  } 
+}
