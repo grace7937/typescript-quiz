cc89370e74255e9696ed780d488eddcd7ab481bd
grace7937 <eunchong.k153@gamil.com>
2021-02-17 14:21:41 +0900

    movegame:done

--- game.ts
+++ game.ts
@@ -1,6 +1,30 @@
 /**
  * Let's make a game 🕹
  */
+
+const position = {x: 0, y: 0};
+type Direction = 'up' | 'down' | 'left' | 'right';
+
+function move(direction: Direction): avoid {
+  switch(direction) {
+    case 'up':
+      position.y += 1;
+      break;
+    case 'down':
+      position.y += 1;
+      break;
+    case 'left':
+      position.x += -1;
+      break;
+    case 'right':
+      position.x += +1;
+      break;
+    default:
+      throw new Error(`unknown direction: ${direction}`);  
+  }
+}
+
+
 console.log(position); // { x: 0, y: 0}
 move('up');
 console.log(position); // { x: 0, y: 1}
@@ -10,3 +34,9 @@ move('left');
 console.log(position); // { x: -1, y: 0}
 move('right');
 console.log(position); // { x: 0, y: 0}
+
+
+
+
+
+
