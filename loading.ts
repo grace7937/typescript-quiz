44ba7acfc6ec73c480109c53fc1a7ee535d8e0d0
grace7937 <eunchong.k153@gamil.com>
2021-02-17 15:00:36 +0900

    typescript-quiz:done

--- loading.ts
+++ loading.ts
@@ -23,4 +23,27 @@
   printLoginState({ state: 'loading' }); // 👀 loading...
   printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
   printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+
+
+function printLoginState(state: ResourceLoadState) {
+  switch(state.state) {
+    case 'loading': 
+    console.log(`${state.state} + '....'`);
+    break;
+
+    case 'success':
+    console.log(`🥳 {$state.response.body}`)
+    break;
+
+    case 'fail':
+    console.log(`😭 ${state.reason}`)
+    break;
+
+    default: 
+    throw new Error(`unknown state: ${state}`);
+
+  }
 }
+
+}
+
